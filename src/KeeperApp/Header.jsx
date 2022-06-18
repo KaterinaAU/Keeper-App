fb31faadefb84b3145091376a95aff0feebb422c
Allen Kate <kateallen92@outlook.com>
2022-06-18 22:31:57 +1000

    first commit

---
+++ src/KeeperApp/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import HighlightOutlinedIcon from '@material-ui/icons//HighlightOutlined';
+
+function Header(){
+    return (
+    <header> 
+    <h1><HighlightOutlinedIcon /> Keeper App</h1>
+    </header>
+    )
+}
+export default Header;
---
+++ src/KeeperApp/Note.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import DeleteIcon from '@material-ui/icons/Delete';
+
+
+function Note(props) {
+
+function DeleteButton() {
+  props.onDelete(props.id);
+}
+
+
+
+    return (
+    <div className="note">
+        <h1>{props.title}</h1>
+        <p>{props.content}</p> 
+        <button onClick={DeleteButton}>
+        <DeleteIcon /> </button>
+        
+    </div>
+    );
+}
+export default Note;
