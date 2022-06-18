7abd356adff12779b5566b516f325a3fe0cb6ef1
Allen Kate <kateallen92@outlook.com>
2022-06-18 23:43:53 +1000

    Deployed webapp

---
+++ build/precache-manifest.be223c0041954de20c18a601a0e56bba.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea5c056d403eb80ecbef448839711ce1",
+    "url": "/Keeper-app/index.html"
+  },
+  {
+    "revision": "29778937a3129789c27f",
+    "url": "/Keeper-app/static/js/2.921c191f.chunk.js"
+  },
+  {
+    "revision": "9a18ef5492ada4cde195",
+    "url": "/Keeper-app/static/js/main.fcb98af7.chunk.js"
+  },
+  {
+    "revision": "33cb2174e8328f4b935a",
+    "url": "/Keeper-app/static/js/runtime-main.b2f67e31.js"
+  }
+]);
