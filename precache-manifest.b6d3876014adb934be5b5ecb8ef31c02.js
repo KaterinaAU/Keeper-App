6a2b00f815e180b360f7a182e532013a5a434d54
Allen Kate <kateallen92@outlook.com>
2022-06-18 23:45:12 +1000

    Updates

---
+++ precache-manifest.b6d3876014adb934be5b5ecb8ef31c02.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ec72988033fd6f0bc97695566c2d3d1",
+    "url": "/Keeper-app/index.html"
+  },
+  {
+    "revision": "29778937a3129789c27f",
+    "url": "/Keeper-app/static/js/2.921c191f.chunk.js"
+  },
+  {
+    "revision": "9a18ef5492ada4cde195",
+    "url": "/Keeper-app/static/js/main.fcb98af7.chunk.js"
+  },
+  {
+    "revision": "33cb2174e8328f4b935a",
+    "url": "/Keeper-app/static/js/runtime-main.b2f67e31.js"
+  }
+]);
